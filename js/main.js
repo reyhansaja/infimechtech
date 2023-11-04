f8398b20489ddb393b6e012c52c829951d57d27e
reyhansaja <raihan.ts16b@gmail.com>
2023-11-04 09:06:05 +0700

    tambah carousel

--- js/main.js
+++ js/main.js
@@ -83,5 +83,15 @@ $(function() {
 		});
 	}; 
 	siteMenuClone();
-
+	$('.owl-1').owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn: 'slideInUp',
+        items:1,
+        loop: true,
+        nav:true,
+        margin:0,
+        stagePadding:0,
+        smartSpeed:450,
+        navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+    });
 });
